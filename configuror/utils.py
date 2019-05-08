dfaca6f96ddbddca59b64ca7333fc2f90d637cf8
lewoudar <lewoudar@gmail.com>
2019-05-08 08:33:51 +0200

    feat(utils.py): implemented functions to extract dict from dotenv files
    added some tests in test_utils.py

--- configuror/utils.py
+++ configuror/utils.py
@@ -1,7 +1,62 @@
 """Helper functions for the project"""
+import re
 from configparser import ConfigParser
+from typing import Dict, List
+
+from .exceptions import DecodeError
+
+SET_EXPORT_EXPRESSION = re.compile(r'\w*\s*(set|export)\s+', flags=re.IGNORECASE)
+ITEM_COMPILED_EXPRESSION = re.compile(r'\w+')
 
 
 def convert_ini_config_to_dict(config: ConfigParser) -> dict:
     """Translates a configparser object into a dict."""
     return {key: dict(value) for key, value in config.items()}
+
+
+# DOTENV
+
+def _sanitize_key_and_value(items: List[str]) -> List[str]:
+    """Removes unwanted characters on key and value"""
+    # if there are more than two items, we return the list unchanged, so an error will be raised later
+    if len(items) != 2:
+        return items
+    key, value = items
+    key = key.rstrip()
+    value = value.lstrip()
+    if (value.startswith('"') and value.endswith('"')) or (value.startswith("'") and value.endswith("'")):
+        value = value[1:-1]
+    return [key, value]
+
+
+def get_dict_from_dotenv_file(filename: str) -> Dict[str, str]:
+    """
+    :param filename: .env file where values are extracted.
+    :return: a dict with keys and values extracted from the .env file.
+    """
+    result_dict = {}
+
+    error_message = 'file {filename}: the line n°{index} is not correct: "{line}"'
+    with open(filename) as f:
+        for index, line in enumerate(f):
+            stripped_line = line.strip()
+            # we don't take into account comments
+            if stripped_line.startswith('#'):
+                continue
+            # we don't take into account empty lines
+            if not stripped_line:
+                continue
+            parts = stripped_line.split('#')  # we remove inline comments if there are any
+            # we remove set or export command if there are any
+            new_line = SET_EXPORT_EXPRESSION.sub('', parts[0].strip())
+
+            # we get key and value
+            parts = new_line.split('=')
+            parts = _sanitize_key_and_value(parts)
+            if len(parts) != 2 or ITEM_COMPILED_EXPRESSION.match(parts[0]) is None \
+                    or ITEM_COMPILED_EXPRESSION.match(parts[1]) is None:
+                line_number = index + 1
+                raise DecodeError(message=error_message.format(filename=filename, index=line_number, line=new_line))
+            result_dict[parts[0]] = parts[1]
+
+    return result_dict
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,9 @@ from configparser import ConfigParser
 
 import pytest
 
-from configuror.utils import convert_ini_config_to_dict
+# noinspection PyProtectedMember
+from configuror.utils import convert_ini_config_to_dict, get_dict_from_dotenv_file, _sanitize_key_and_value
+from configuror.exceptions import DecodeError
 
 
 @pytest.fixture()
@@ -30,3 +32,74 @@ class TestConvertIniConfigToDict:
         for item in ['databases', 'servers.alpha', 'servers.beta']:
             assert item in result_dict
         assert '192.168.1.1' == result_dict['databases']['server']
+
+
+class TestSanitizeKeyAndValue:
+    """Tests function _sanitize_key_and_value"""
+
+    @pytest.mark.parametrize('items', [
+        [],
+        ['foo'],
+        ['foo', 'bar', 'char']
+    ])
+    def test_should_return_the_same_list_if_items_length_is_not_equal_to_two(self, items):
+        assert items == _sanitize_key_and_value(items)
+
+    @pytest.mark.parametrize(('given_items', 'expected_items'), [
+        (['key', 'value'], ['key', 'value']),
+        (['key  ', '  value'], ['key', 'value']),
+        (['key', "o'clock"], ['key', "o'clock"]),
+        (['key', '"o\'clock"'], ['key', "o'clock"]),
+        (['key', "'Big Mom'"], ['key', 'Big Mom'])
+    ])
+    def test_should_return_correct_sanitized_items(self, given_items, expected_items):
+        assert expected_items == _sanitize_key_and_value(given_items)
+
+
+class TestGetDictFromDotEnvFile:
+    """tests function get_dict_from_dotenv_file"""
+
+    def test_should_return_correct_dict_when_parsing_dotenv_file(self, tmp_path):
+        content_lines = [
+            '# a first comment',
+            'set foo=bar',
+            'char = var',
+            '     ',
+            'EXPORT tic =tac  # a second comment',
+            '# another comment',
+            'SET_FOO  = CLIMB',
+            'exportCHAR=  paint',
+            '  too = much ',
+            'NAME="Kevin T"'
+        ]
+        expected_dict = {
+            'foo': 'bar',
+            'char': 'var',
+            'tic': 'tac',
+            'SET_FOO': 'CLIMB',
+            'exportCHAR': 'paint',
+            'too': 'much',
+            'NAME': 'Kevin T'
+        }
+        path = tmp_path / '.env'
+        path.write_text('\n'.join(content_lines))
+
+        assert expected_dict == get_dict_from_dotenv_file(path)
+
+    @pytest.mark.parametrize('invalid_line', [
+        'set=foo=bar',
+        'foo =',
+        '= bar'
+    ])
+    def test_should_raise_error_when_syntax_is_incorrect(self, tmp_path, invalid_line):
+        content_lines = [
+            'export NAME=FOO',
+            invalid_line
+        ]
+        path = tmp_path / '.env'
+        path.write_text('\n'.join(content_lines))
+
+        with pytest.raises(DecodeError) as exc_info:
+            get_dict_from_dotenv_file(path)
+
+        assert f'file {path}: the line n°2 is not correct: "{invalid_line}"' == str(exc_info.value)
