b765bd144337179cde533578875b87ae833fbc0d
lewoudar <lewoudar@gmail.com>
2019-12-29 18:58:55 +0100

    ci: updated noxfile.py, appveyor.yml and .travis.yml
    noxfile.py: added support for multiple python versions in the tests session and pinned a specific python version in other sessions. Fixed an issue with usage of shutil.rmtree method
    .travis.yml: updated travis configuration file to take in account the new specifications of the noxfile
    appveyor.yml: updated appveyor configuration file to take in account the
    new specifications of the noxfile

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,20 @@
 # Changelog
 
+## Version 0.X.X
+
+Released on ????-??-??
+
+- Added usage of nox package for test automation.
+- Changed .travis.yml and appveyor.yml to take in account nox.
+
 ## Version 0.1.1
 
 Released on 2019-05-15
 
 ### Bug fixes
 
-- Changed the deploy phase in .travis.yml
-- Added the link to the documentation in README.md
+- Changed the deploy phase in .travis.yml.
+- Added the link to the documentation in README.md.
 
 ## Version 0.1.0
 
--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,14 @@
 import shutil
+import os
 
 import nox
 
 nox.options.reuse_existing_virtualenvs = True
 
+PYTHON_VERSIONS = ['3.6', '3.7', '3.8']
 
-@nox.session
+
+@nox.session(python=PYTHON_VERSIONS[-1])
 def lint(session):
     """Performs pep8 and security checks."""
     source_code = 'configuror'
@@ -14,29 +17,48 @@ def lint(session):
     session.run('bandit', '-r', source_code)
 
 
-@nox.session
+@nox.session(python=PYTHON_VERSIONS)
 def tests(session):
     """Runs the test suite."""
     session.install('pyyaml', 'toml', 'pytest', 'pytest-cov', 'pytest-mock')
     session.cd('tests')
     session.run('pytest')
 
+    # we notify codecov when the latest version of python is used
+    if session.python == PYTHON_VERSIONS[-1] and 'APPVEYOR_URL' not in os.environ:
+        session.notify('codecov')
+
 
 @nox.session
-def coverage(session):
+def codecov(session):
     """Runs codecov command to share coverage information on codecov.io"""
     session.install('codecov')
-    session.run('codecov')
+    session.cd('tests')
+    session.run('coverage', 'xml', '-i')
+    session.run('codecov', '-f', 'coverage.xml')
 
 
-@nox.session
+@nox.session(python=PYTHON_VERSIONS[-1])
 def docs(session):
     """Builds the documentation."""
     session.install('mkdocs')
     session.run('mkdocs', 'build', '--clean')
 
 
+@nox.session
+def deploy(session):
+    """
+    Deploys build on pypi repositories (can be test or production repository).
+    Extra arguments passed to nox will be passed to the twine command.
+    """
+    if not session.posargs and not session.interactive:
+        session.error("you don't pass arguments to session and you are not in interactive mode")
+    session.install('-U', 'twine', 'setuptools', 'wheel')
+    session.run('python', 'setup.py', 'sdist', 'bdist_wheel')
+    session.run('twine', 'upload', 'dist/*', *session.posargs)
+
+
 @nox.session(python=False)
 def clean(*_):
     """Since nox take a bit of memory, this command helps to clean nox environment."""
-    shutil.rmtree('.nox')
+    shutil.rmtree('.nox', ignore_errors=True)
